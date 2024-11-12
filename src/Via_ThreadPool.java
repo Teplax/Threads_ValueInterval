519b23499902b5fae8bbce0fc6bba08cf4d405d2
Aleksandr Teplyakov <teplax@gmail.com>
2024-11-12 19:06:50 +0300

    ThreadPool working!

--- src/Via_ThreadPool.java
+++ src/Via_ThreadPool.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-
-public class Via_Threads {
-    public static void main(String[] args) throws InterruptedException {
-
+public class Via_ThreadPool {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
@@ -13,13 +16,12 @@ public class Via_Threads {
 
 
         long startTs = System.currentTimeMillis(); // start time
-//        Callable<String> myCallable = new
-//        ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
-
-        List<Thread> threads = new ArrayList<>(); // создаём список потоков
+        ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
+        List<Future<Integer>> futures = new ArrayList<>(texts.length);
         for (String text : texts) { //запускаем цикл по созданным строкам
-            threads.add( //добавляем потоки в список
-                    new Thread( //создаём для обработки каждой строки отдельный поток
+            futures.add(
+            threadPool.submit( //добавляем потоки в список
+                     //создаём для обработки каждой строки отдельный поток
                             () -> { //передаём реализацию интерфейса Runnable через лямбда-функцию
                                 int maxSize = 0;
                                 for (int i = 0; i < text.length(); i++) {
@@ -39,19 +41,20 @@ public class Via_Threads {
                                         }
                                     }
                                 }
-                                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                                return(maxSize);
                             }
                     )
             );
 
         }
-        for (Thread thread : threads) { //запускаем потоки из списка
-            thread.start();
+        List<Integer> results = new ArrayList<>();
+        for (Future<Integer> future: futures) { //запускаем потоки из списка
+            results.add(future.get());
         }
-        for (Thread thread : threads) { //ожидание запущенных потоков основным потоком программы
-            thread.join();
-        }
+        results.sort(Comparator.naturalOrder());
+        System.out.println(results.getLast());
         long endTs = System.currentTimeMillis(); // end time
+        threadPool.shutdown();
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
@@ -64,4 +67,5 @@ public class Via_Threads {
         }
         return text.toString();
     }
+
 }
